85a3a9f1792039424e5dafc06aba7f07377f2c92
Mohammed Nafees <hello@mnafees.me>
2025-08-04 17:24:12 +0200

    Fix env loader for a first time user (#284)
    * fix env loader for a first time user
    
    * add info about .env

--- hatchet_client/hatchet_client.go
+++ hatchet_client/hatchet_client.go
@@ -1,14 +1,26 @@
 package hatchet_client
 
 import (
+	"errors"
+	"os"
+
 	v1 "github.com/hatchet-dev/hatchet/pkg/v1"
 	"github.com/joho/godotenv"
 )
 
 func HatchetClient() (v1.HatchetClient, error) {
-	err := godotenv.Load()
-	if err != nil {
-		return nil, err
+	if _, err := os.Stat(".env"); os.IsExist(err) {
+		err := godotenv.Load()
+		if err != nil {
+			return nil, err
+		}
 	}
+
+	// check for HATCHET_CLIENT_TOKEN
+	token := os.Getenv("HATCHET_CLIENT_TOKEN")
+	if token == "" {
+		return nil, errors.New("HATCHET_CLIENT_TOKEN is not set")
+	}
+
 	return v1.NewHatchetClient()
 }
